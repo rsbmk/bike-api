fb36cab020cfb9dac3bbcfc4a1a939a7c8c2913e
Roberto Bocio Melo <robertobocio@01lab.co>
2023-07-03 09:34:35 -0500

    change: read envs

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+import "https://deno.land/std@0.192.0/dotenv/load.ts"; // upload envs
 import { Application } from "https://deno.land/x/oak@v12.5.0/mod.ts";
 
 
@@ -20,6 +21,8 @@ app.use(rentrouter.allowedMethods());
 
 // deno run -A https://deno.land/x/denon/denon.ts run -A main.ts 
 
+const port = Deno.env.get("PORT") || 3000;
+
 console.log("Server running on port 3000");
-await app.listen({ port: 3000 })
+await app.listen({ port: +port })
 
--- modules/Connection.ts
+++ modules/Connection.ts
@@ -1,14 +1,10 @@
-import { load } from "https://deno.land/std@0.192.0/dotenv/mod.ts";
-
-const env = await load();
-
 import { Client } from "https://deno.land/x/mysql@v2.11.0/mod.ts";
 
 export const client = await new Client().connect({
-  hostname: env["hostname"],
-  username: env["username"],
-  password: env["password"],
-  db: env["db"],
-  port: parseInt(env["port"]),
+  hostname: Deno.env.get("hostname"),
+  username: Deno.env.get("username"),
+  password: Deno.env.get("password"),
+  db: Deno.env.get("db"),
+  port: parseInt(Deno.env.get("port") || "3306"),
 });
 
